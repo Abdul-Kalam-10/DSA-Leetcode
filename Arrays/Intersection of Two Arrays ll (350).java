7f3a77dce2f38b7b829764456334e5e86caf0faf
Abdul Kalam B <106393720+Abdul-Kalam-10@users.noreply.github.com>
2023-11-17 21:57:08 +0530

    Update Intersection of Two Arrays ll (350).java

--- Arrays/Intersection of Two Arrays ll (350).java
+++ Arrays/Intersection of Two Arrays ll (350).java
@@ -5,7 +5,7 @@ class Solution {
     int i=0,j=0,k=0;
     while(i<nums1.length && j<nums2.length){
         if(nums1[i]==nums2[j]){
-            nums1[k++] = nums1[i];
+            nums1[k++] = nums1[i];   
             i++;j++;
         }
         else if(nums1[i]>nums2[j])
