d8a6c5c1a931052a53fc1f3e23b634df0b9e2a4f
Abdul Kalam B <106393720+Abdul-Kalam-10@users.noreply.github.com>
2024-01-26 20:29:51 +0530

    Update Arranging Coins (441).java

--- Math/Arranging Coins (441).java
+++ Math/Arranging Coins (441).java
@@ -1,11 +1,11 @@
 class Solution {
     public int arrangeCoins(int n) {
-        int i = 1;
-        while(n>=i)
+        int i = 1; //i init to 1
+        while(n>=i) //while cond
         {
-            n = n-i;
-            i = i+1;
+            n = n-i; //n dec by 1
+            i = i+1; //i inc by 1
         }
-        return i-1; 
+        return i-1; ///return i-1
     }
-}
+}  //end
