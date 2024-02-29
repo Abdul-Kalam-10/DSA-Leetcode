f7cc34ebfda966f3f4407d986f682f6c2406545d
Abdul Kalam B <106393720+Abdul-Kalam-10@users.noreply.github.com>
2024-02-29 22:07:28 +0530

    Add files via upload

---
+++ Linked List/Remove Linked List Elements (203).java
@@ -0,0 +1,22 @@
+class Solution {
+    public ListNode removeElements(ListNode head, int val) {
+
+        if(head == null) return null; //when head is null
+        
+        ListNode dummy = new ListNode(-1); //dummy node created
+        dummy.next = head;  //dummy before head
+
+        ListNode curr = dummy; //curr pointer points dummy
+
+        while(curr.next != null){  //till it reaches null
+            if(curr.next.val == val){   //curr.next matches val
+                curr.next = curr.next.next;  //skip 1 node
+            }
+            else{
+                curr = curr.next;  //normal iteration
+            }
+
+        }
+        return dummy.next;  //dummy.next linked list without val
+    }
+}
