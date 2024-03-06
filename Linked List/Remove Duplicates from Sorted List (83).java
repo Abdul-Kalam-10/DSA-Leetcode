ff43cfd011d2140bf8cb82586d2de4dba76f2dad
Abdul Kalam B <106393720+Abdul-Kalam-10@users.noreply.github.com>
2024-03-06 18:47:04 +0530

    Add files via upload

---
+++ Linked List/Remove Duplicates from Sorted List (83).java
@@ -0,0 +1,18 @@
+class Solution {
+    public ListNode deleteDuplicates(ListNode head) {
+          
+        ListNode curr = head;  //curr points head
+
+        while(curr != null && curr.next != null){  //while cond
+            if(curr.val == curr.next.val){   //current.value == current.next.value(Duplicate)
+                curr.next = curr.next.next;  //skip that node and .next.next
+            }
+
+            else{
+                curr = curr.next;  //othercase no same value move .next
+            }
+        }
+
+        return head; //return head with remove duplicate node
+    }
+}
